7a54418527c8917e36cffbb980c4d274716529b5
Misbah-Mansuri <101227223+Misbah-Mansuri@users.noreply.github.com>
2022-03-09 12:37:32 +1100

    Create foobar.php

---
+++ foobar.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Print 1 to 100 numbers divisible by 3 ,5 and both add write foo,bar and foobar respectively
+
+
+for($num=1; $num<= 100; $num++)
+{
+	if($um % 3 == 0 AND $num % 5 == 0)
+  {
+    echo "foobar";
+  }
+}
+
+?>
